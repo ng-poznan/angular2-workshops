8ec762fd3a8deb4ba0d01f68782e472c3ea57431
Kamil Augustynowicz <kamil.augustynowicz@gmail.com>
2017-02-05 22:04:36 +0100

    update angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,16 +18,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'ds works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ds works!');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('ds works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Death Star');
   }));
 });
