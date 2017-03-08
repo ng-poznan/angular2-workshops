cee7363dab711b49a0075c7af0c1723b0e0199c0
Daniel Koza <dakolech@gmail.com>
2017-03-08 15:58:42 +0100

    move stormtroopers

--- src/app/turbolift/barracks/barracks.component.ts
+++ src/app/turbolift/barracks/barracks.component.ts
@@ -1,31 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-const stormtroopers = [
-  {
-    inHelmet: 'stormtrooper.jpg',
-    withoutHelmet: 'jango.png',
-  },
-  {
-    inHelmet: 'stormtrooper.jpg',
-    withoutHelmet: 'jango.png',
-  },
-  {
-    inHelmet: 'stormtrooper.jpg',
-    withoutHelmet: 'HanSoloStormtrooper.jpg',
-  },
-  {
-    inHelmet: 'stormtrooper.jpg',
-    withoutHelmet: 'jango.png',
-  },
-  {
-    inHelmet: 'stormtrooper.jpg',
-    withoutHelmet: 'luke-stormtrooper.jpg',
-  },
-  {
-    inHelmet: 'stormtrooper.jpg',
-    withoutHelmet: 'jango.png',
-  },
-];
+import { stormtroopers } from './data';
 
 @Component({
   selector: 'ds-barracks',
---
+++ src/app/turbolift/barracks/data.ts
@@ -0,0 +1,26 @@
+export const stormtroopers = [
+  {
+    inHelmet: 'stormtrooper.jpg',
+    withoutHelmet: 'jango.png',
+  },
+  {
+    inHelmet: 'stormtrooper.jpg',
+    withoutHelmet: 'jango.png',
+  },
+  {
+    inHelmet: 'stormtrooper.jpg',
+    withoutHelmet: 'HanSoloStormtrooper.jpg',
+  },
+  {
+    inHelmet: 'stormtrooper.jpg',
+    withoutHelmet: 'jango.png',
+  },
+  {
+    inHelmet: 'stormtrooper.jpg',
+    withoutHelmet: 'luke-stormtrooper.jpg',
+  },
+  {
+    inHelmet: 'stormtrooper.jpg',
+    withoutHelmet: 'jango.png',
+  },
+];
