b94a87c0c655d00f8f534b4c5c95d29fdc153281
Daniel Koza <dakolech@gmail.com>
2017-03-09 09:45:09 +0100

    console.log lifecycle hooks

--- src/app/turbolift/barracks/barracks.component.ts
+++ src/app/turbolift/barracks/barracks.component.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked, OnChanges } from '@angular/core';
 import { stormtroopers } from './data';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'ds-barracks',
   templateUrl: './barracks.component.html',
   styleUrls: ['./barracks.component.scss']
 })
-export class BarracksComponent implements OnInit {
-  public stormtroopers = stormtroopers;
-  public saidValue = '';
+export class BarracksComponent implements OnInit, AfterViewInit, AfterViewChecked, OnChanges {
+  public index = 0;
+  public stormtrooper = stormtroopers[this.index];
+  public stringValue = 'Empire!!!';
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
+    console.log('Barracks: OnChanges');
   }
 
-  onSaySomething(val) {
-    this.saidValue = val;
+  ngOnInit() {
+    console.log('Barracks: OnInit');
+
+    setTimeout(() => {
+      this.stringValue = 'Empire!';
+      console.log('Barracks setTimeout called');
+    }, 2000);
+  }
+
+  ngAfterViewInit() {
+    console.log('Barracks: AfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('Barracks: AfterViewChecked');
+  }
+
+  assignNewStormtrooper() {
+    this.index = this.index === stormtroopers.length ? 0 : this.index + 1;
+    this.stormtrooper = stormtroopers[this.index];
   }
 
 }
--- src/app/turbolift/barracks/stormtrooper/stormtrooper.component.ts
+++ src/app/turbolift/barracks/stormtrooper/stormtrooper.component.ts
@@ -1,25 +1,43 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges, OnChanges,
+  DoCheck, AfterContentInit, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'ds-stormtrooper',
   templateUrl: './stormtrooper.component.html',
   styleUrls: ['./stormtrooper.component.scss']
 })
-export class StormtrooperComponent implements OnInit {
-  @Input() value: any;
-  @Output() saySomething = new EventEmitter<string>();
+export class StormtrooperComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked {
+  @Input() value;
   public inHelmet = true;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('Stormtrooper: OnInit');
+
+    setTimeout(() => {
+      this.inHelmet = false;
+      console.log('Stormtrooper setTimeout called');
+    }, 4000);
   }
 
-  toggleHelmet() {
-    this.inHelmet = !this.inHelmet;
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Stormtrooper: OnChanges', changes);
   }
 
-  imagePath(val) {
-    return `./assets/images/${val}`;
+  ngDoCheck() {
+    console.log('Stormtrooper: DoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('Stormtrooper: AfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('Stormtrooper: AfterContentChecked');
+  }
+
+  imagePath(val = { withoutHelmet: ''}) {
+    return `./assets/images/${val.withoutHelmet}`;
   }
 }
