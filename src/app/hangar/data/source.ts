6e03a7c4f92776c4ba6ba07e83e2cdd04dbf9b9b
miroslaw.bogacz <miroslaw.bogacz@hotmail.com>
2017-04-26 21:26:42 +0200

    update hangar template

--- src/app/hangar/data/source.ts
+++ src/app/hangar/data/source.ts
@@ -6,5 +6,48 @@ export const source = {
     { value: 3, label: 'TIE Vanguard' },
     { value: 4, label: 'TIE Fire Control' },
     { value: 5, label: 'TIE Bomber' }
-  ]
+  ],
+
+  data: {
+    ships: [
+      {
+        model: 0,
+        maxSpeed: 2030,
+        price: 3361923,
+        weight: 746391,
+        description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse',
+        weapons: [
+          { name: 'Lorem ipsum 1', power: 320, weight: 120 },
+          { name: 'Dolor sit 2', power: 140, weight: 78 },
+          { name: 'Etiam ullamcorper 3', power: 600, weight: 421 },
+        ]
+      },
+
+      {
+        model: 2,
+        maxSpeed: 67821,
+        price: 1958271,
+        weight: 420329,
+        description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse',
+        weapons: [
+          { name: 'Lorem ipsum 4', power: 420, weight: 220 },
+          { name: 'Dolor sit 5', power: 40, weight: 15 },
+          { name: 'Etiam ullamcorper 6', power: 542, weight: 382 },
+        ]
+      },
+
+      {
+        model: 4,
+        maxSpeed: 4300,
+        price: 947391,
+        weight: 74831,
+        description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse',
+        weapons: [
+          { name: 'Lorem ipsum 7', power: 892, weight: 768 },
+          { name: 'Dolor sit 8', power: 271, weight: 293 },
+          { name: 'Etiam ullamcorper 9', power: 884, weight: 213 },
+        ]
+      }
+    ],
+  }
 };
