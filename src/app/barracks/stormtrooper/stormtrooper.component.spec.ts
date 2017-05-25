4d4723d53e240d5b5af4b2b506024d4518837e16
Daniel Koza <dakolech@gmail.com>
2017-05-25 16:46:54 +0200

    tests

--- src/app/barracks/stormtrooper/stormtrooper.component.spec.ts
+++ src/app/barracks/stormtrooper/stormtrooper.component.spec.ts
@@ -1,27 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StormtrooperComponent } from './stormtrooper.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import {Component, NO_ERRORS_SCHEMA} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('StormtrooperComponent', () => {
-  let component: StormtrooperComponent;
-  let fixture: ComponentFixture<StormtrooperComponent>;
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  const mockedStormtrooper = {
+    name: 'Storm one',
+    badge: {
+      color: 'darkred',
+      rank: 'Sgt.',
+    }
+  };
+
+  @Component({
+    moduleId: module.id,
+    selector: 'ds-test',
+    template: `<ds-stormtrooper [stormtrooper]="stormtrooper"></ds-stormtrooper>`
+  })
+  class TestComponent {
+    stormtrooper = mockedStormtrooper;
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StormtrooperComponent ],
+      declarations: [ StormtrooperComponent, TestComponent ],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(StormtrooperComponent);
+    fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should pass stormtrooper properties to badge', function () {
+    expect(fixture.debugElement.query(By.css('ds-badge')).properties['badge']).toEqual(mockedStormtrooper.badge);
+  });
+
+  it('should render name', function () {
+    expect(fixture.debugElement.query(By.css('ds-badge')).nativeElement.textContent).toBe(mockedStormtrooper.name);
   });
 });
